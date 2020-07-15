106a09a20d464f834ce536e8afda3fb72bd3c5ec
8977shiva <b.skreddy1997@gmail.com>
2020-07-16 02:50:20 +0530

    jsx

--- AdoptMe/src/app.js
+++ AdoptMe/src/app.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet  from "./Pet";
+import Pet from "./Pet";
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt me"),
-    React.createElement(Pet, { name: "Tom", animal: "Cat" }),
-    React.createElement(Pet, { name: "Oggy", animal: "Cat" }),
-    React.createElement(Pet, { name: "Boxer", animal: "Dog" })
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h1", {}, "Adopt me"),
+  //   React.createElement(Pet, { name: "Tom", animal: "Cat" }),
+  //   React.createElement(Pet, { name: "Oggy", animal: "Cat" }),
+  //   React.createElement(Pet, { name: "Boxer", animal: "Dog" })
+  // );
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me</h1>
+      <Pet name="tom" animal="cat" />
+      <Pet name="Oggy" animal="cat" />
+      <Pet name="Boxer" animal="Dog" />
+    </div>
   );
 };
 
 // Rendring the function to the dom
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
